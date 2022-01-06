44a8e7ea027621e5588fd350b1c3a9fa57583033
PIPALIYA JAYDIP <pipaliyaj3@gmail.com>
2022-01-06 14:45:07 +0530

    Update student_data.php

--- student_data.php
+++ student_data.php
@@ -6,16 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Student Data</title>
+    <style>table {
+  border-collapse: collapse;
+  width: 100%;
+}
+th{
+    background-color:lightgreen;
+    text-align: center;
+}
+th, td {
+  padding: 8px;
+  
+  border-bottom: 1px solid #DDD;
+}
+tr:hover {background-color: #D6EEEE;}
+</style>
 </head>
 <body>
+    <?php 
+
+    //server connection
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname="student";
+
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+    
+    //check connection between site and database
+    if(!$conn){
+        die("Connection failed".mysqli_connect_error());
+    }
+    else{
+        echo "Connected Successfull";
+    }
+
+    //form inputdata add to main dabase table
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $enno = $_POST["enno"];
+    $gen = $_POST["gender"];
+    $exmrec = $_POST["exmrec"];
+    $conno = $_POST["contactno"];
+    
+    //for sql query run on server
+    $sql="INSERT INTO roll_no (FirstName,LastName,EnNO,Gender,ExamRecord,ContactNo) VALUES ('$fname','$lname','$enno','$gen','$exmrec','$conno')";
+    $result = mysqli_query($conn,$sql);
+    if(!$result){
+        echo 'failes';
+    }
+    
+    ?>
     <h1>Student Data</h1>
-    <table border="solid black 2px">
+    <div class="container">
+    <table  border = " solid black 2px ">
         <tr>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>En. NO</th>
+            <th>En.NO</th>
             <th>Gender</th>
             <th>Exam Record</th>
+            <th>Contact No</th>
         </tr>
         <tr>
             <td><?php echo $_POST["fname"]; ?></td>
@@ -23,8 +74,11 @@
             <td><?php echo $_POST["enno"]; ?></td>
             <td><?php echo $_POST["gender"];?></td>
             <td><?php echo $_POST["exmrec"]; ?></td>
+            <td><?php echo $_POST["contactno"]?></td>
         </tr>
-        
     </table>
+    </div>
+    <hr>
+    <center><a href="index.php"><button> MY Data is <strong>Correct</strong></button></a></center>
 </body>
 </html>
